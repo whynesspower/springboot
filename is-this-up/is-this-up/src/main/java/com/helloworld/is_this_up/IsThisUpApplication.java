e21ca0cd9d00c538e6ba6dcaedcf2b54961f3c4c
whynesspower <yashrajshukla48@gmail.com>
2024-06-04 22:43:40 +0530

    feat(initial): scaffolding files

---
+++ is-this-up/is-this-up/src/main/java/com/helloworld/is_this_up/IsThisUpApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.is_this_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsThisUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsThisUpApplication.class, args);
+	}
+
+}
---
+++ is-this-up/is-this-up/src/test/java/com/helloworld/is_this_up/IsThisUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.is_this_up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsThisUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
