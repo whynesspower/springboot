b47ef6d6944425bae0d81e8cea3230bcdacdc496
whynesspower <yashrajshukla48@gmail.com>
2024-06-04 22:53:35 +0530

    feat(api): adding try catch and some constants

--- is-this-up/is-this-up/src/main/java/controllers/UrlCheckController.java
+++ is-this-up/is-this-up/src/main/java/controllers/UrlCheckController.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,8 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class UrlCheckController {
 
+    private final String IS_SITE_UP="Site is up";
+    private final String SIDE_IS_DOWN= "Site is down :(";
+    private final String INCORRECT_URL= "Url is not correct";
+
     @GetMapping("/check")
     public String getUrlStatusMessage(@RequestParam String urlString) {
-        return new String();
+        String returnMessage="";
+        try {
+            URL urlObject= new URL(urlString);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return returnMessage;
     }
 }
